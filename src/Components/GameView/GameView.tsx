40ccf8199ee83d76fbf7f9cd196907f7d5345c46
Emilia Starczyk <emiliastarczyk@gmail.com>
2022-06-30 22:49:26 +0200

    Game Status

--- src/Components/GameView/GameView.tsx
+++ src/Components/GameView/GameView.tsx
@@ -1,63 +1,49 @@
 import CustomButton from 'Components/CustomButton/CustomButton';
-import { Game } from 'Logic/Game';
+import { Game, GameStatus } from 'Logic/Game';
 import { useEffect, useRef, useState } from 'react';
 import { BOARD_SIZE } from 'Utils/GameUtils';
 import styles from './GameView.module.css';
 import { PlayFill, PauseFill, XLg } from 'react-bootstrap-icons';
 
-type GameStatus = {
-    isInPlay: boolean,
-    ended: boolean,
-    isPaused: boolean,
-}
+
 
 export default function GameView() {
     const [game, setGame] = useState<Game | undefined>(undefined);
     const [gameStatus, setGameStatus] = useState<GameStatus>({
-        isInPlay: false,
-        ended: false,
+        isPlaying: false,
+        isEnd: false,
         isPaused: false,
+        score: 0,
     })
+
     const canvasRef = useRef<HTMLCanvasElement>(null);
 
     useEffect(() => {
         const canvas = canvasRef.current
         const context = canvas?.getContext('2d')
         if (context) {
-            setGame(new Game(context));
+            setGame(new Game(context, gameStatusChanged));
         }
     }, [])
 
-    function runGame() {
-        game?.startGame();
-        setGameStatus({
-            isInPlay: true,
-            ended: false,
-            isPaused: false,
-        })
-    }
-
     function startGame() {
-        setTimeout(() => runGame(), 2000);
+        setTimeout(() => game?.startGame(), 2000);
     }
 
     function resetGame() {
-        game?.resetGame();
-        setGameStatus({
-            isInPlay: false,
-            ended: false,
-            isPaused: false,
-        })
-        setTimeout(() => runGame(), 2000);
+        setTimeout(() => game?.resetGame(), 2000);
     }
 
     function pauseGame() {
         game?.pauseGame();
-        setGameStatus({
-            isInPlay: false,
-            ended: false,
-            isPaused: true,
-        })
+    }
+
+    function continueGame() {
+        setTimeout(() => game?.startGame(true), 2000);
+    }
+
+    function gameStatusChanged(currentGameStatus: GameStatus) {
+        setGameStatus(currentGameStatus);
     }
 
     function onBoardKeyDown(e: React.KeyboardEvent<HTMLDivElement>) {
@@ -66,12 +52,25 @@ export default function GameView() {
 
     return (
         <div className={styles.gameBox} onKeyDown={onBoardKeyDown} tabIndex={0}>
+            <div className={styles.infoBox}></div>
             <canvas className={styles.board} ref={canvasRef} height={BOARD_SIZE} width={BOARD_SIZE}></canvas>
-            <div className={styles.buttonOverlay}>
-                {!gameStatus.isInPlay && <CustomButton value="Play" onClick={startGame} className={styles.playButton} contentClassName={styles.buttonContent} icon={<PlayFill height={80} width={80} />} />}
-                {gameStatus.ended && <CustomButton value="Restart game" onClick={resetGame} className={styles.restartButton} contentClassName={styles.buttonContent} icon={<XLg height={80} width={80} />} />}
-            </div>
-            {gameStatus.isInPlay && !gameStatus.isPaused && <CustomButton value="Pause" onClick={pauseGame} className={styles.pauseButton} icon={<PauseFill height={30} width={30} />} />}
+            {(!gameStatus.isPlaying || gameStatus.isEnd) &&
+                <div className={styles.buttonOverlay}>
+                    {!gameStatus.isPlaying &&
+                        <CustomButton value="Play" onClick={startGame} contentClassName={styles.buttonContent} icon={<PlayFill height={80} width={80} />} />
+                    }
+                    {gameStatus.isEnd &&
+                        <CustomButton value="Restart game" onClick={resetGame} contentClassName={styles.buttonContent} icon={<XLg height={80} width={80} />} />
+                    }
+                </div>
+            }
+            {gameStatus.isPlaying && !gameStatus.isPaused &&
+                <CustomButton value="Pause" onClick={pauseGame} icon={<PauseFill height={30} width={30} />} />
+            }
+
+            {gameStatus.isPlaying && gameStatus.isPaused &&
+                <CustomButton value="Continue" onClick={continueGame} icon={<PauseFill height={30} width={30} />} />
+            }
         </div>
     )
 }
--- src/Logic/Game.ts
+++ src/Logic/Game.ts
@@ -4,31 +4,54 @@ import { Apple } from "./Apple";
 import { Bombs } from "./Bombs";
 import { Snake } from "./Snake";
 
+export type GameStatus = {
+    isPlaying: boolean,
+    isPaused: boolean,
+    isEnd: boolean,
+    score: number,
+}
+
 export class Game {
     apple: Apple;
     bombs: Bombs;
     snake: Snake;
     timer: NodeJS.Timer | undefined;
     context: CanvasRenderingContext2D;
+    gameStatus: GameStatus;
+    gameStatusChanged: (currentGameStatus: GameStatus) => void;
 
-    constructor(context: CanvasRenderingContext2D) {
+    constructor(context: CanvasRenderingContext2D, gameStatusChanged: (currentGameStatus: GameStatus) => void) {
         this.apple = new Apple(context);
         this.bombs = new Bombs(context);
         this.snake = new Snake(context);
         this.context = context;
+        this.gameStatusChanged = gameStatusChanged;
+        this.gameStatus = {
+            isPlaying: false,
+            isPaused: false,
+            isEnd: false,
+            score: 0,
+        }
     }
 
-    startGame() {
+    startGame(withCallbackOfResume: boolean = false) {
         if (this.timer === undefined) {
             this.apple.start();
             this.snake.start();
             this.bombs.start();
             this.timer = setInterval(this.updateGameLoop.bind(this), 1000 / 60);
+            this.gameStatus.isPlaying = true;
+            this.gameStatus.isPaused = false;
+            if (withCallbackOfResume) {
+                this.gameStatusChanged(this.gameStatus);
+            }
         }
     }
 
     pauseGame() {
         if (this.timer !== undefined) {
+            this.gameStatus.isPaused = true;
+            this.gameStatusChanged(this.gameStatus);
             this.apple.pause();
             this.snake.pause();
             this.bombs.pause();
@@ -44,6 +67,27 @@ export class Game {
         this.snake.reset();
         this.bombs.reset();
         this.context.clearRect(0, 0, 1024, 1024)
+        this.startGame();
+    }
+
+    updateGameLoop() {
+        this.context.clearRect(0, 0, 1024, 1024);
+        this.apple.update();
+        this.snake.update();
+        this.bombs.update();
+
+        this.checkColision();
+    }
+
+    endGame() {
+        if (this.timer !== undefined) {
+            this.gameStatus.isEnd = true;
+            this.gameStatusChanged(this.gameStatus);
+            clearInterval(this.timer);
+            this.timer = undefined;
+            this.context.clearRect(0, 0, 1024, 1024);
+        }
+
     }
 
     checkColision() {
@@ -59,6 +103,8 @@ export class Game {
         if (this.checkTwoPointsCollision(snakeHead, applePosiion)) {
             this.snake.makeLonger();
             this.apple.setIsChangePositionExpected(true);
+            this.gameStatus.score += 1;
+            this.gameStatusChanged(this.gameStatus);
             return true;
         }
         return false;
@@ -108,15 +154,6 @@ export class Game {
         return true;
     }
 
-    updateGameLoop() {
-        this.context.clearRect(0, 0, 1024, 1024);
-        this.apple.update();
-        this.snake.update();
-        this.bombs.update();
-
-        this.checkColision();
-    }
-
     reactToUserInput(key: string) {
         switch (key) {
             case "ArrowDown": {
@@ -136,18 +173,11 @@ export class Game {
                 break;
             }
             case "Escape": {
-                this.pauseGame();
+                this.gameStatus.isPaused = !this.gameStatus.isPaused;
+                if (this.gameStatus.isPaused) this.startGame(true);
+                else this.pauseGame();
                 break;
             }
-        }
-    }
-
-    endGame() {
-        console.log('end');
-        if (this.timer !== undefined) {
-            clearInterval(this.timer);
-            this.timer = undefined;
-            this.context.clearRect(0, 0, 1024, 1024);
         }
     }
 }
