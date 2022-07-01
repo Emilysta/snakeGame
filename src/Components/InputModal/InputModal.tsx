17c1c3babd13da5a337f94a484574f9343a49c20
Emilia <emiliastarczyk@gmail.com>
2022-07-01 17:48:58 +0200

    hide with blend - fix

--- src/Components/InputModal/InputModal.tsx
+++ src/Components/InputModal/InputModal.tsx
@@ -12,8 +12,8 @@ export default function InputModal(props: InputModalProps) {
     const [inputValue, setInputValue] = useState(props.defaultValue ? props.defaultValue : "player");
 
     return (
-        <div className={styles.blend}>
-            <div className={`${styles.modal} ${props.isShown ? styles.show : ''}`}>
+        <div className={`${styles.blend} ${props.isShown ? styles.show : ''}`}>
+            <div className={styles.modal}>
                 <label htmlFor='playerName' className={styles.textInputLabel}>{props.label ? props.label : "Player name: "}</label>
                 <input className={styles.textInput} name="playerName" value={inputValue} onChange={(e) => setInputValue(e.target.value)} />
                 <input className={styles.submitInputButton} type="submit" value="OK" onClick={() => props.onSubmit(inputValue)} />
