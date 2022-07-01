8983720a85e8aec98e0cb62ad4628185059a4fee
Emilia <emiliastarczyk@gmail.com>
2022-07-01 17:43:57 +0200

    InputModal style

--- src/Components/InputModal/InputModal.tsx
+++ src/Components/InputModal/InputModal.tsx
@@ -4,16 +4,20 @@ import styles from './InputModal.module.css';
 type InputModalProps = {
     onSubmit: (inputValue: string) => void,
     isShown: boolean,
+    defaultValue?: string;
+    label?: string;
 }
 
 export default function InputModal(props: InputModalProps) {
-    const [inputValue, setInputValue] = useState("player");
+    const [inputValue, setInputValue] = useState(props.defaultValue ? props.defaultValue : "player");
 
     return (
-        <div className={props.isShown ? styles.modalShown : styles.modalHidden}>
-            <label htmlFor='playerName'>Player name:</label><br></br>
-            <input type={'text'} name="playerName" value={inputValue} onChange={(e) => setInputValue(e.target.value)} />
-            <input type="submit" value="OK" onClick={()=>props.onSubmit(inputValue)} />
+        <div className={styles.blend}>
+            <div className={`${styles.modal} ${props.isShown ? styles.show : ''}`}>
+                <label htmlFor='playerName' className={styles.textInputLabel}>{props.label ? props.label : "Player name: "}</label>
+                <input className={styles.textInput} name="playerName" value={inputValue} onChange={(e) => setInputValue(e.target.value)} />
+                <input className={styles.submitInputButton} type="submit" value="OK" onClick={() => props.onSubmit(inputValue)} />
+            </div>
         </div>
     )
 }
